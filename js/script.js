eab7e9e099912bdc59067a5a81eb889df4665e4b
anders-max <ajimbo309@gmail.com>
2019-10-28 22:07:10 +0000

    10_28_19 finish some script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
+$(".output").text("hey");
+var nums= $("input").val();
+var nums_array = nums.split('+');
+})
+function pig(str)
+{
+    return str;
+}
 
-
-
-  
-  
+  $("button").click(() => {
+          let message = $("input").val();
+          $("body").append(message);
+  })
   
   
 });
